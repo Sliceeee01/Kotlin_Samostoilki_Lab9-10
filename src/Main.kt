35c296e3ff99f7f909d92ae54dfc8584143056ee
Sliceeee01 <ivan_hih@icloud.com>
2026-04-08 17:37:57 +0300

    Demonstrate equals, copy and toString for Task

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,35 @@
-fun main() {
-    val employee = Employee("Богданов Иван Антонович", "Разработчик", 100000, 5)
+//fun main() {
+//    val employee = Employee("Богданов Иван Антонович", "Разработчик", 100000, 5)
+//
+//    println(employee.getInfo())
+//    employee.setSalary(-50000)
+//    employee.setSalary(120000)
+//
+//    employee.setYearsOfExperience(-3)
+//    employee.setYearsOfExperience(60)
+//    employee.setYearsOfExperience(8)
+//    println(employee.getInfo())
+//}
 
-    println(employee.getInfo())
-    employee.setSalary(-50000)
-    employee.setSalary(120000)
+fun testDataClass() {
+    val task1 = Task("Исправить баг", "Исправить ошибку в авторизации", Priority.HIGH)
+    val task2 = Task("Исправить баг", "Исправить ошибку в авторизации", Priority.HIGH)
+    val task3 = Task("Написать тесты", "Написать unit-тесты для модуля", Priority.MEDIUM, true)
 
-    employee.setYearsOfExperience(-3)
-    employee.setYearsOfExperience(60)
-    employee.setYearsOfExperience(8)
-    println(employee.getInfo())
+    println("1. Демонстрация equals():")
+    println("   task1 == task2: ${task1 == task2}")  // true
+    println("   task1 == task3: ${task1 == task3}")  // false
+
+    println("   task1 hashCode: ${task1.hashCode()}")
+    println("   task2 hashCode: ${task2.hashCode()}")
+    println("   task3 hashCode: ${task3.hashCode()}")
+
+    println("   $task1")
+    println("   $task3")
+
+    val task1HighPriority = task1.copy(priority = Priority.HIGH)
+    val task1Completed = task1.copy(isCompleted = true)
+    println("   Оригинал: $task1")
+    println("   Копия с изменением приоритета: $task1HighPriority")
+    println("   Копия с отметкой о выполнении: $task1Completed")
 }
