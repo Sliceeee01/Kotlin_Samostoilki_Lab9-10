33989d9a4d1b106639a4190bff3f1199700f2774
Sliceeee01 <ivan_hih@icloud.com>
2026-04-08 17:50:34 +0300

    Implement DevelopmentDepartment and TestingDepartment

--- src/Main.kt
+++ src/Main.kt
@@ -11,25 +11,34 @@
 //    println(employee.getInfo())
 //}
 
-fun testDataClass() {
-    val task1 = Task("Исправить баг", "Исправить ошибку в авторизации", Priority.HIGH)
-    val task2 = Task("Исправить баг", "Исправить ошибку в авторизации", Priority.HIGH)
-    val task3 = Task("Написать тесты", "Написать unit-тесты для модуля", Priority.MEDIUM, true)
+//fun testDataClass() {
+//    val task1 = Task("Исправить баг", "Исправить ошибку в авторизации", Priority.HIGH)
+//    val task2 = Task("Исправить баг", "Исправить ошибку в авторизации", Priority.HIGH)
+//    val task3 = Task("Написать тесты", "Написать unit-тесты для модуля", Priority.MEDIUM, true)
+//
+//    println("1. Демонстрация equals():")
+//    println("   task1 == task2: ${task1 == task2}")  // true
+//    println("   task1 == task3: ${task1 == task3}")  // false
+//
+//    println("   task1 hashCode: ${task1.hashCode()}")
+//    println("   task2 hashCode: ${task2.hashCode()}")
+//    println("   task3 hashCode: ${task3.hashCode()}")
+//
+//    println("   $task1")
+//    println("   $task3")
+//
+//    val task1HighPriority = task1.copy(priority = Priority.HIGH)
+//    val task1Completed = task1.copy(isCompleted = true)
+//    println("   Оригинал: $task1")
+//    println("   Копия с изменением приоритета: $task1HighPriority")
+//    println("   Копия с отметкой о выполнении: $task1Completed")
+//}
+fun testDepartments() {
+    println("\n=== Демонстрация абстрактных классов ===\n")
 
-    println("1. Демонстрация equals():")
-    println("   task1 == task2: ${task1 == task2}")  // true
-    println("   task1 == task3: ${task1 == task3}")  // false
+    val devDept = DevelopmentDepartment()
+    val testDept = TestingDepartment()
 
-    println("   task1 hashCode: ${task1.hashCode()}")
-    println("   task2 hashCode: ${task2.hashCode()}")
-    println("   task3 hashCode: ${task3.hashCode()}")
-
-    println("   $task1")
-    println("   $task3")
-
-    val task1HighPriority = task1.copy(priority = Priority.HIGH)
-    val task1Completed = task1.copy(isCompleted = true)
-    println("   Оригинал: $task1")
-    println("   Копия с изменением приоритета: $task1HighPriority")
-    println("   Копия с отметкой о выполнении: $task1Completed")
+    devDept.printDepartmentGoal()
+    testDept.printDepartmentGoal()
 }
